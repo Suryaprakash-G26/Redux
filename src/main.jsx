5d1b9a7391d0713790eafa78bb2e023fea261fbc
GS26Surya <gsurya124@gmail.com>
2023-11-14 15:10:36 +0530

    Minor changes

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
-import AppContext from './contetapi/contextapi.jsx'
 import { configureStore } from '@reduxjs/toolkit'
 import { Provider } from 'react-redux'
 import redux from './Redux reducers/redux.js'
@@ -16,9 +15,9 @@ const store=configureStore({
 ReactDOM.createRoot(document.getElementById('root')).render(
     <BrowserRouter>
     <Provider store={store}>
-        <AppContext >
-            <App />
-        </AppContext>
+        
+       <App />
+      
     </Provider>
         
     </BrowserRouter>
